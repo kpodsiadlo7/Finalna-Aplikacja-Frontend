95ec2a159ea4009ad70857231f9fc97688a259df
Karol Podsiadlo <karolekk131@gmail.com>
2022-11-30 20:25:42 +0100

    Create MainView

---
+++ src/main/java/com/clinic/privateclinic/MainView.java
@@ -0,0 +1,11 @@
+package com.clinic.privateclinic;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+
+public class MainView extends VerticalLayout {
+
+    public MainView(){
+
+    }
+
+}
