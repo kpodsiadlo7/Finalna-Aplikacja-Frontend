32197bcf2dfd5bbd6aae51d77630a0d135a16f41
Karol Podsiadlo <karolekk131@gmail.com>
2022-11-30 20:13:24 +0100

    first commit

---
+++ src/main/java/com/clinic/privateclinic/PrivateclinicApplication.java
@@ -0,0 +1,13 @@
+package com.clinic.privateclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateclinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinic/privateclinic/PrivateclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinic.privateclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateclinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
